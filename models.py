ecbddb0e33fde93c02b87b03db7a54693f21d957
shallon <shallonmokeira@gmail.com>
2023-10-05 07:31:20 +0300

    add tables to database

--- models.py
+++ models.py
@@ -7,8 +7,6 @@ engine = create_engine('sqlite:///inventory.db')
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
-
 
 class Category(Base):
     __tablename__ = 'categories'
@@ -23,6 +21,7 @@ class Inventory(Base):
     id = Column(Integer, primary_key=True)
     product_id = Column(Integer, ForeignKey('products.id'))
     quantity = Column(Integer)
+    product = relationship("Product", back_populates="inventory")
 
 class Product(Base):
     __tablename__ = 'products'
@@ -42,3 +41,4 @@ class Product(Base):
         self.price = price
         self.category = category
         
+Base.metadata.create_all(engine)
